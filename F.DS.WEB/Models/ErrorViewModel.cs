b806d5fd11c721039bcecff02f7cbe003a830722
baderan <amir0hossin0pr@gmail.com>
2020-12-10 13:24:54 +0330

    insert Pwa insert Mvc Insert Web Projects

---
+++ F.DS.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace F.DS.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
